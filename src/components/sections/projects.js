30b8964064c970f2e3a3bbbed549319868d7fc8d
Siddhi-Nargund <snargun1@jhu.edu>
2020-12-11 03:59:41 -0500

    Project 12

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -252,7 +252,9 @@ const Projects = () => {
           })}
       </TransitionGroup>
 
-     
+      <button className="more-button" onClick={() => setShowMore(!showMore)}>
+        Show {showMore ? 'Less' : 'More'}
+      </button>
     </StyledProjectsSection>
   );
 };
