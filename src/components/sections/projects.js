a6cce87db41d4fb5f3801f0c952bc20622ef8968
ksreenu1 <73123570+ksreenu1@users.noreply.github.com>
2021-04-04 18:13:32 -0400

    Update projects.js

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -252,9 +252,6 @@ const Projects = () => {
           })}
       </TransitionGroup>
 
-      <button className="more-button" onClick={() => setShowMore(!showMore)}>
-        Show {showMore ? 'Less' : 'More'}
-      </button>
     </StyledProjectsSection>
   );
 };
